1f3d7282e26393e7e64272c9e3215d52c6156e6f
Nikhat Parvin <nikhat.parvin013@gmail.com>
2024-09-29 21:22:10 +0530

    Done

--- PROJECTS/07-Myntra_Clone/03-Myntra_React-Clone/src/store/index.js
+++ PROJECTS/07-Myntra_Clone/03-Myntra_React-Clone/src/store/index.js
@@ -1,10 +1,12 @@
 import {configureStore, createSlice} from "@reduxjs/toolkit";
 import itemsSlice from "./itemsSlice";
+import fetchStatusSlice from "./fetchStatusSlice";
 
 
 const  Myntrastore = configureStore({
   reducer:{
-    items: itemsSlice.reducer
+    items: itemsSlice.reducer,
+    fetchStatus : fetchStatusSlice.reducer,
   }
 })
 
